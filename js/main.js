8e4351e422b8d333e979c093a5087278a9ea9908
mamartam <127197633+mamartam@users.noreply.github.com>
2026-01-22 18:27:30 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,15 +5,16 @@ const socMediaContainer = document.querySelector(
 const hide = document.querySelector(
   ".component__general-information-container--user-information",
 );
-const triangle = document.querySelector(".triangle");
 const footer = document.querySelector(".footer");
-// let viewportWidth = window.innerWidth;
+
 seeMoreBtn.addEventListener("click", () => {
   socMediaContainer.classList.toggle("seeMore");
   if (window.innerWidth < 700) {
     hide.classList.toggle("hide");
+
     footer.classList.toggle("backgroundChanger");
   } else if (window.innerWidth > 700) {
-    triangle.classList.toggle("effSeeMore");
+    hide.classList.toggle("hide");
+    footer.classList.toggle("backgroundChanger");
   }
 });
